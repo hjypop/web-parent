6a6a83ff61bcbfc1166081c5f7dca2f43ef85161
Yangcl <794867067@qq.com>
2016-06-21 00:02:05 +0800

    存储过程测试代码 提交 | 锁表基本操作完成 WebHelper.java存在注入问题

--- web-core/src/test/java/com/hjy/core/test/LockTest.java
+++ web-core/src/test/java/com/hjy/core/test/LockTest.java
@@ -1,19 +1,24 @@
 package com.hjy.core.test;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.hjy.helper.WebHelper;
+import com.hjy.service.ILockService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/applicationContext.xml", "classpath:/mybatis-config.xml" })
 public class LockTest {
+	@Resource
+	private ILockService lockService;
 	
 	@Test
 	public void test() {
-		String uuid = WebHelper.getInstance().addLock("test", 10);
-		System.out.println(uuid);
+		String aaa = lockService.addLock("keycode22", 10, "UUID23243");
+		 
+		System.out.println("hello world " + aaa);  
 	}
 }
