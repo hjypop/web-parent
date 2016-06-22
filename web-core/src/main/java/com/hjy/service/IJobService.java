1a43beba786ec6cb969ccaaf6daf4a10e311abac
yangchenglin <yangchenglin@huijiayou.cn>
2016-06-22 15:29:42 +0800

    16

--- web-core/src/main/java/com/hjy/service/IJobService.java
+++ web-core/src/main/java/com/hjy/service/IJobService.java
@@ -1,7 +1,23 @@
 package com.hjy.service;
 
+import java.util.List;
+
 import com.hjy.pojo.entity.system.JobExectimer;
 
 public interface IJobService  extends IBaseService<JobExectimer , Integer> {
-
+	
+	public List<JobExectimer> listJobExectimer(JobExectimer entity);
+	
+	public Integer updateJobExectimer(JobExectimer entity);
 }
+
+
+
+
+
+
+
+
+
+
+
--- web-core/src/main/java/com/hjy/service/impl/JobServiceImpl.java
+++ web-core/src/main/java/com/hjy/service/impl/JobServiceImpl.java
@@ -1,5 +1,7 @@
 package com.hjy.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
@@ -8,10 +10,18 @@ import com.hjy.dao.IJobExectimerDao;
 import com.hjy.pojo.entity.system.JobExectimer;
 import com.hjy.service.IJobService;
 
-@Service("systemService")
+@Service("jobService")
 public class JobServiceImpl extends BaseServiceImpl<JobExectimer, Integer> implements IJobService {
 	@Resource
 	private IJobExectimerDao jobExectimerDao;
+
+	public List<JobExectimer> listJobExectimer(JobExectimer entity) {
+		return jobExectimerDao.findList(entity); 
+	}
+
+	public Integer updateJobExectimer(JobExectimer entity) {
+		return jobExectimerDao.updateSelective(entity); 
+	}
 	
 	
 	
