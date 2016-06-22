86f76c604593fa2e6278a18649b25f7d74e06b1e
yangchenglin <yangchenglin@huijiayou.cn>
2016-06-22 13:30:37 +0800

    JobService

---
+++ web-core/src/main/java/com/hjy/dao/IJobExectimerDao.java
@@ -0,0 +1,7 @@
+package com.hjy.dao;
+
+import com.hjy.entity.system.JobExectimer;
+
+public interface IJobExectimerDao extends BaseDao<JobExectimer, Integer> {
+
+}
---
+++ web-core/src/main/java/com/hjy/entity/system/JobExectimer.java
@@ -0,0 +1,94 @@
+package com.hjy.entity.system;
+
+import java.util.Date;
+
+public class JobExectimer {
+    private Integer zid;
+    private String uid;
+    private String execCode;
+    private String execType;
+    private String execInfo;
+    private String beginTime;
+    private String endTime;
+    private String execTime;
+    private Integer flagSuccess;
+    private Integer execNumber;
+    private String remark;
+    private Date createTime;
+    
+    
+	public Integer getZid() {
+		return zid;
+	}
+	public void setZid(Integer zid) {
+		this.zid = zid;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getExecCode() {
+		return execCode;
+	}
+	public void setExecCode(String execCode) {
+		this.execCode = execCode;
+	}
+	public String getExecType() {
+		return execType;
+	}
+	public void setExecType(String execType) {
+		this.execType = execType;
+	}
+	public String getExecInfo() {
+		return execInfo;
+	}
+	public void setExecInfo(String execInfo) {
+		this.execInfo = execInfo;
+	}
+	public String getBeginTime() {
+		return beginTime;
+	}
+	public void setBeginTime(String beginTime) {
+		this.beginTime = beginTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public String getExecTime() {
+		return execTime;
+	}
+	public void setExecTime(String execTime) {
+		this.execTime = execTime;
+	}
+	public Integer getFlagSuccess() {
+		return flagSuccess;
+	}
+	public void setFlagSuccess(Integer flagSuccess) {
+		this.flagSuccess = flagSuccess;
+	}
+	public Integer getExecNumber() {
+		return execNumber;
+	}
+	public void setExecNumber(Integer execNumber) {
+		this.execNumber = execNumber;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+    
+    
+}
