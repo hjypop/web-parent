25e4a75d427b721414e504bc56f12e0a660ec15e
yangchenglin <yangchenglin@huijiayou.cn>
2016-06-22 18:27:01 +0800

    SysJob 相关

--- web-core/src/main/java/com/hjy/service/IJobService.java
+++ web-core/src/main/java/com/hjy/service/IJobService.java
@@ -3,12 +3,15 @@ package com.hjy.service;
 import java.util.List;
 
 import com.hjy.pojo.entity.system.JobExectimer;
+import com.hjy.pojo.entity.system.SysJob;
 
 public interface IJobService  extends IBaseService<JobExectimer , Integer> {
 	
 	public List<JobExectimer> listJobExectimer(JobExectimer entity);
 	
 	public Integer updateJobExectimer(JobExectimer entity);
+	
+	public Integer updateSysJobByUuid(SysJob entity);
 }
 
 
--- web-quartz/src/main/java/com/hjy/quartz/job/RootJobForExec.java
+++ web-quartz/src/main/java/com/hjy/quartz/job/RootJobForExec.java
@@ -1,6 +1,5 @@
 package com.hjy.quartz.job;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
