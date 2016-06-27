cf16dec56ed46573c429178f497a8f21ee71283a
yangchenglin <yangchenglin@huijiayou.cn>
2016-06-27 18:21:14 +0800

    2

--- seller-adapter/src/main/java/com/hjy/dao/system/IScStoreSkunumDao.java
+++ seller-adapter/src/main/java/com/hjy/dao/system/IScStoreSkunumDao.java
@@ -1,5 +1,9 @@
 package com.hjy.dao.system;
 
-public interface IScStoreSkunumDao {
+import com.hjy.dao.BaseDao;
+import com.hjy.entity.product.PcSkuinfo;
+import com.hjy.entity.system.ScStoreSkunum;
+
+public interface IScStoreSkunumDao extends BaseDao<ScStoreSkunum , Integer> {
 
 }
