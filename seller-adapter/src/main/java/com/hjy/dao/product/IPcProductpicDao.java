391af5c1deecc3ba6d63085abf8d85bb66121469
yangchenglin <yangchenglin@huijiayou.cn>
2016-06-28 15:12:29 +0800

    2

--- seller-adapter/src/main/java/com/hjy/dao/product/IPcProductpicDao.java
+++ seller-adapter/src/main/java/com/hjy/dao/product/IPcProductpicDao.java
@@ -1,8 +1,18 @@
 package com.hjy.dao.product;
 
+import java.util.List;
+
 import com.hjy.dao.BaseDao;
 import com.hjy.entity.product.PcProductpic;
 
 public interface IPcProductpicDao extends BaseDao<PcProductpic , Integer> {
-
+	/**
+	 * @descriptions query("", "", "product_code=:product_code  and (sku_code='' or sku_code is null)", pcPicListMapParam, -1, -1);
+	 * 
+	 * @param entity
+	 * @date 2016年6月28日下午2:51:32
+	 * @author Yangcl 
+	 * @version 1.0.0.1
+	 */
+	 public List<PcProductpic> findListBySkuNull(PcProductpic entity); 
 }
