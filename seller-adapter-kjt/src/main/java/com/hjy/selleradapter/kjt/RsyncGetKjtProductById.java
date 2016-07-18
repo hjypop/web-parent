4f339a3f07c133b703bdc646b8c01c6b6bc42ebf
zhanghaiyu <zhanghy@huijiayou.cn>
2016-07-18 16:39:27 +0800

    跨境通信息变化的商品入库相关BUG修改

--- seller-adapter-kjt/src/main/java/com/hjy/selleradapter/kjt/RsyncGetKjtProductById.java
+++ seller-adapter-kjt/src/main/java/com/hjy/selleradapter/kjt/RsyncGetKjtProductById.java
@@ -145,7 +145,6 @@ public class RsyncGetKjtProductById
 					try {
 						productinfo.getProductSkuInfoList().get(0).setCostPrice(new BigDecimal(p1));
 					} catch (Exception e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
