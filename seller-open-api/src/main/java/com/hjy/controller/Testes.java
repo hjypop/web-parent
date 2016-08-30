a829d7128b3a79f8ac1c343ecdb71c9474083be0
Yangcl <794867067@qq.com>
2016-08-30 21:52:14 +0800

    2

--- seller-open-api/src/main/java/com/hjy/controller/Testes.java
+++ seller-open-api/src/main/java/com/hjy/controller/Testes.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.util.SystemOutLogger;
 
 import com.alibaba.fastjson.JSON;
 import com.hjy.annotation.ExculdeNullField;
@@ -18,44 +17,27 @@ import com.hjy.request.data.OrderInfoInsert;
 public class Testes {
 
 	public static void main(String[] args) { 
+		
 		new Testes().aaa(); 
+		
+		
 	}
-	
-	
 	
 	public void aaa(){
 		List<OrderInfoInsert> list = null;
 		try {
 			list = JSON.parseArray(DataInit.orderInfoBatchInsertTest().getData(), OrderInfoInsert.class);
 		} catch (Exception e) { 
-			e.printStackTrace(); 
-		}
-		
-		List<OcOrderinfo> oList = new ArrayList<>();
-		for(OrderInfoInsert i : list){
-			OcOrderinfo e = new OcOrderinfo(); 
-			this.validate(i, e);
 			
-			oList.add(e);
 		}
-		
-		System.out.println("|||||||||||||||||||||||||||||"); 
+		for(OrderInfoInsert i : list){
+			OcOrderinfo e = new OcOrderinfo();
+			e.setOrderCode("asdfadsf"); 
+			this.validate(i, e);
+		}
 	}
 
 	
-	
-	
-	/**
-	 * @descriptions  验证对象中的值是否合法并赋值
-	 * 
-	 * @param t 要验证的对象
-	 * @param entity 要赋值的对象
-	 * @return entity
-	 * @date 2016年8月29日下午12:10:38
-	 * @author Yangcl 
-	 * @version 1.0.0.1
-	 * @param <E>
-	 */
 	private  <T , E> boolean validate(T t , E e){
 		Field[] fields = t.getClass().getDeclaredFields();
 		 try {
@@ -65,40 +47,41 @@ public class Testes {
 				 String func = "get" + name.substring(0,1).toUpperCase()+name.substring(1);
 				 Method m = t.getClass().getMethod(func);
 	             String value = String.valueOf(m.invoke(t)); 
-	             if(value.equals("null")) {
-	            	 value = null;
-	             }
 	             if( !field.isAnnotationPresent(ExculdeNullField.class) && StringUtils.isBlank(value) ){    
-	            	 // 不包含此标签则是必传字段，如果没传值则认为此条记录错误
-	            	 return false; 
-	             }else if(field.isAnnotationPresent(ExculdeNullField.class) && StringUtils.isBlank(value) ){
-	            	 // ExculdeNullField注解标识的字段为空，则不再对其反射设值。
-	            	 continue;
-	             }
+	            	 // 如果包含此标签则排除该字段的验证
+	            	 return false;
+	             } 
 	             
 	             // 赋值 
-	             String func_ = "set" + name.substring(0,1).toUpperCase()+name.substring(1); 
-				 Method m_ = e.getClass().getMethod(func_  , m.invoke(t).getClass());
+	             String func_ = "set" + name.substring(0,1).toUpperCase()+name.substring(1);
+				 Method m_ = t.getClass().getMethod(func_  , m.invoke(t).getClass());
 				 @SuppressWarnings("rawtypes")
 				Class[] c = m_.getParameterTypes();
 				 if(c[0] == String.class) {
 		 	 		 m_.invoke(e , value);
+		 	 		 break;
 		 	 	 }else if(c[0] == BigDecimal.class) {
-		 	 		 m_.invoke(e , BigDecimal.valueOf(Double.valueOf(value)));
+		 	 		 m_.invoke(e , BigDecimal.valueOf(Long.valueOf(value)));
+		 	 		 break;
 		 	 	 }else if(c[0] == Integer.class) {
 					 m_.invoke(e ,Integer.valueOf(value));
+					 break;
 			 	 }else if(c[0] == Boolean.class) {
 		 	 		 m_.invoke(e , Boolean.valueOf(value));
+		 	 		 break;
 		 	 	 }else if(c[0] == Float.class){
 		 		 	 m_.invoke(e , Float.valueOf(value));
+                     break;
 		 	 	 }else if(c[0] == Double.class) {
 	 	 		 	 m_.invoke(e , Double.valueOf(value));
+	 	 		 	 break;
 		 	 	 }else if(c[0] == Byte.class) {
 		 	 		 m_.invoke(e , Byte.valueOf(value));
+		 	 		 break;
 		 	 	 }
 			 }
 		 } catch (NoSuchMethodException ex) {
-//			 ex.printStackTrace();  不做处理即可
+			 ex.printStackTrace();
 		 } catch (SecurityException ex) {
 			 ex.printStackTrace();
 		 } catch (IllegalAccessException ex) {
