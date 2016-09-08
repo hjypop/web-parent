d24b86a4d5009a77939494622edb7062fd81afd9
yangchenglin <yangchenglin@huijiayou.cn>
2016-09-08 09:51:38 +0800

    2

--- seller-adapter-minspc/src/main/java/com/hjy/selleradapter/minspc/RsyncProductList.java
+++ seller-adapter-minspc/src/main/java/com/hjy/selleradapter/minspc/RsyncProductList.java
@@ -9,10 +9,13 @@ public class RsyncProductList extends RsyncMinspc {
 		return "Subscribe.ProductList";
 	}
 
-	@Override
+	
+	//	Status：-1 （当前有库存的全部订阅产品列表）
+	//	Status：0 （当前有库存的保税贸易订阅产品列表）
+	//	Status：1 （当前有库存的海外直邮订阅产品列表）
+	//	Status：2 （当前有库存的一般贸易订阅产品列表）
 	public String setRequestDataJson() {
-		// TODO Auto-generated method stub
-		return null;
+		return "{\"status\":\"-1\"}";
 	}
 
 	@Override
