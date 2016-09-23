df772b78c4b98f4c059a3e37ad2211d6ca5c6eab
yangchenglin <yangchenglin@huijiayou.cn>
2016-09-23 15:44:47 +0800

    获取订单状态优化完毕

--- seller-adapter-minspc/src/main/java/com/hjy/selleradapter/minspc/RsyncOrderStatusList.java
+++ seller-adapter-minspc/src/main/java/com/hjy/selleradapter/minspc/RsyncOrderStatusList.java
@@ -3,17 +3,21 @@ package com.hjy.selleradapter.minspc;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.alibaba.fastjson.JSON;
 import com.hjy.annotation.Inject;
 import com.hjy.dao.order.IOcKjSellerSeparateOrderDao;
+import com.hjy.dao.order.IOcOrderShipmentsDao;
 import com.hjy.dao.order.IOcOrderinfoDao;
 import com.hjy.dto.response.orderStatus.DataResponse;
 import com.hjy.dto.response.orderStatus.SoResponse;
 import com.hjy.entity.order.OcKjSellerSeparateOrder;
+import com.hjy.entity.order.OcOrderShipments;
 import com.hjy.entity.order.OcOrderinfo;
+import com.hjy.helper.DateHelper;
 import com.hjy.helper.ExceptionHelper;
 
 public class RsyncOrderStatusList extends RsyncMinspc {
@@ -24,6 +28,8 @@ public class RsyncOrderStatusList extends RsyncMinspc {
 	private IOcOrderinfoDao orderinfoDao;
 	@Inject 
 	private IOcKjSellerSeparateOrderDao kjSellerSeparateOrderDao; 
+	@Inject
+	private IOcOrderShipmentsDao ocOrderShipmentsDao;
 	
 	private List<OcKjSellerSeparateOrder> list;
 	
@@ -36,13 +42,20 @@ public class RsyncOrderStatusList extends RsyncMinspc {
 			logger.error(message);
 			return message; 
 		} 
+
 		String msg = "";
-		if(entity.getCode().equals("0")){
-			this.updateOrderStatus(entity.getData()); 
-			msg = "Rsync Order Status Success";
-		}else{
-			msg = entity.getDesc();
+		try {
+			if(entity.getCode().equals("0")){
+				this.updateOrderStatus(entity.getData()); 
+				msg = "Rsync Order Status Success";
+			}else{
+				msg = entity.getDesc();
+			}
+		} catch (Exception e) {
+			msg = "获取订单状态失败：\n";
+			msg += ExceptionHelper.allExceptionInformation(e);
 		}
+		
 		return msg;
 	}
 
@@ -62,7 +75,7 @@ public class RsyncOrderStatusList extends RsyncMinspc {
 	public String getRequestDataJson() {
 		OcKjSellerSeparateOrder e = new OcKjSellerSeparateOrder();
 		e.setSellerCode(this.getSellerCode()); 
-		e.setSellerStatus("1"); 
+		e.setSellerStatus("0"); // 0: 还未出关
 		list = kjSellerSeparateOrderDao.selectByTypes(e);
 		List<String> list_ = new ArrayList<>();
 		for(OcKjSellerSeparateOrder o : list){
@@ -84,9 +97,9 @@ public class RsyncOrderStatusList extends RsyncMinspc {
 	private void updateOrderStatus(List<DataResponse> list_){
 		List<OcOrderinfo> orderinfoList = new ArrayList<>(); // 保存已发货的订单信息 
 		for(DataResponse e : list_){
-			OcOrderinfo info = new OcOrderinfo();
 			OcKjSellerSeparateOrder k = new OcKjSellerSeparateOrder();
 			if(e.getSoStatus().equals("1")){ // 已出关 则认为已经发货 
+				OcOrderinfo info = new OcOrderinfo();
 				for(OcKjSellerSeparateOrder o : list){    // 保存oc_orderInfo表中的订单编号，等待更新真正的订单状态
 					if(e.getOrderID().equals(o.getSellerOrderCode())){
 						info.setOrderCode(o.getOrderCode());
@@ -95,17 +108,30 @@ public class RsyncOrderStatusList extends RsyncMinspc {
 					}
 				}
 				k.setStatus("4497153900010003"); // order_status 订单状态 已发货
+				// 更新拆单表
+				k.setSellerStatus(e.getSoStatus()); 
+				k.setUpdateTime(new Date());
+				k.setSellerOrderCode(e.getOrderID()); 		// 更新依据
+				k.setSellerCode(this.getSellerCode()); 		// 更新依据 
+				kjSellerSeparateOrderDao.updateBySellerOrderCode(k);
 				
-				// TODO 插入物流信息
-				String expressBill = e.getExpressBill(); // 民生品粹返回的快递单号
-				
+				// 插入物流信息
+				String [] arr = this.getLogisticse(e.getShipTypeID()).split("@");
+				OcOrderShipments oos = new OcOrderShipments();
+				oos.setOrderCode(info.getOrderCode());
+				if(arr.length == 2){
+					oos.setLogisticseCode(arr[0]); 
+					oos.setLogisticseName(arr[1]); 
+				}else{
+					oos.setLogisticseCode("民生品粹未返回快递公司编号"); 
+					oos.setLogisticseName("无"); 
+				}
+				oos.setWaybill(e.getExpressBill());
+				oos.setCreator("minspc-job-system"); 
+				oos.setCreateTime(DateHelper.formatDate(new Date())); 
+				oos.setRemark("您的订单已出海关"); 
+				ocOrderShipmentsDao.insertSelective(oos);
 			}
-			// 更新拆单表
-			k.setSellerStatus(e.getSoStatus()); 
-			k.setSellerOrderCode(e.getOrderID()); 
-			k.setUpdateTime(new Date());
-			k.setSellerCode(this.getSellerCode()); 
-			kjSellerSeparateOrderDao.updateBySellerOrderCode(k);
 		}
 		
 		// 批量更新oc_orderInfo表中的状态  
@@ -115,6 +141,42 @@ public class RsyncOrderStatusList extends RsyncMinspc {
 	}
 	
 	
+	/**
+	 * @description: 根据民生品粹 shipTypeId 返回 logisticseCode@logisticseName 
+	 *
+	 * @param code
+	 * 
+	 * @author Yangcl
+	 * @date 2016年9月14日 下午4:20:39
+	 * @version 1.0.0.1
+	 */
+	private String getLogisticse(String code){
+		String result = "";
+		switch (code) {
+			case "375":
+				result = "yunda@韵达";
+				break;
+			case "84":
+				result = "rufengda@如风达";
+				break;
+			case "1":
+				result = "shunfeng@顺丰速运";
+				break;
+			case "2":
+				result = "yuantong@圆通速递";
+				break;
+			case "93":
+				result = "shentong@申通";
+				break;
+			case "264":
+				result = "youzhengxb@邮政小包";
+				break;
+			case "393":
+				result = "tiantian@天天速递";
+				break; 
+		}
+		return result;
+	}
 
 }
 
