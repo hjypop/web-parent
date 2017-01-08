11a6791febc95a79e05ab012f56c65977895f4b6
yangchenglin <yangchenglin@huijiayou.cn>
2017-01-08 19:31:48 +0800

    lc_open_api_seller_product_operations

---
+++ seller-open-api/src/main/java/com/hjy/entity/log/LcOpenApiSellerProductOperations.java
@@ -0,0 +1,19 @@
+package com.hjy.entity.log;
+
+import java.util.Date;
+
+public class LcOpenApiSellerProductOperations {
+    private Integer zid;
+    private String uid;
+    private String sellerCode;
+    private String apiName;
+    private String classUrl;
+    private Date createTime;
+    private String requestData;
+    private String handleData;
+    private String responseData;
+    private String remark;
+    
+    
+    
+}
